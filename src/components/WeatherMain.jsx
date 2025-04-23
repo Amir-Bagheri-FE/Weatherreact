38ee5e10ddd61948685c4bf6060c1635bacdf3b2
AmirBagheri <amiramb10@gmail.com>
2025-04-23 21:14:22 +0330

    styles on weather main

--- src/components/WeatherMain.jsx
+++ src/components/WeatherMain.jsx
@@ -2,20 +2,21 @@ function WeatherMain() {
   return (
     <div
       id="main"
-      className="w-[70%] h-full bg-[rgba(55,176,255,0.34)] rounded-2xl shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[7px] border border-[rgba(55,176,255,0.3)]"
+      className="flex flex-col justify-between p-1.5 w-[70%] h-full bg-blue-200 bg-opacity-30 rounded-2xl backdrop-blur-md border border-blue-900"
     >
-      <div className="flex flex-col justify-center items-center w-[50%] h-2.5">
-        <h2>please search for a city</h2>
+      <div className="flex flex-col justify-center items-center w-full h-auto py-4">
+        <h2 className="text-gray-700 font-semibold">Please search for a city</h2>
         <img
           src="http://openweathermap.org/img/wn/01d@2x.png"
-          className="animation-pulse"
-          alt=""
+          className="animate-pulse mt-2"
+          alt="Weather Icon"
         />
       </div>
-      <div className="w-full h-[200px] bg-[rgba(55,176,255,0.34)] backdrop-blur-[7px]">
-        <div></div>
-        <div></div>
-        <div></div>
+
+      <div className="w-full h-48 bg-blue-200 bg-opacity-30 backdrop-blur-md mt-4 rounded-lg flex justify-around items-center">
+        <div className="w-[25%] h-[70%] m-0.5   bg-white rounded-xl shadow-md"></div>
+        <div className="w-[50%] h-[70%] m-0.5 bg-white rounded-xl shadow-md"></div>
+        <div className="w-[25%] h-[70%] m-0.5 bg-white rounded-xl shadow-md"></div>
       </div>
     </div>
   );
