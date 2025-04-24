df3485f83ad4c9071c76bca304833e6334bfd96a
AmirBagheri <amiramb10@gmail.com>
2025-04-24 13:15:57 +0330

    trend city component

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,11 @@
 import SearchBar from "../sections/SearchBar";
+import TrendCity from "../sections/TrendCity";
 
 function Details() {
   return (
     <div className="w-[30%] p-3 h-full bg-gradient-to-br from-blue-500 via-blue-400 to-blue-600 rounded-3xl">
       <SearchBar />
+      <TrendCity />
     </div>
   );
 }
