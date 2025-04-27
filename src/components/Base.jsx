ebe1ed7ed70af8e53a466e3acb6b5c1dbef1f930
AmirBagheri <amiramb10@gmail.com>
2025-04-27 11:51:11 +0330

    API call in base component

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,11 +1,24 @@
 import Details from "./Details";
 import WeatherMain from "./WeatherMain";
 
+import.meta.env.VITE_WEATHER_API_KEY;
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
 function Base() {
+  async function APIcall(city) {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      const data = await res.json();
+    } catch (error) {
+      console.error(error, "error occurred");
+    }
+  }
   return (
     <div className="flex h-[100vh]  w-full bg-cover bg-center bg-no-repeat">
      <WeatherMain />
-     <Details />
+     <Details callAPI={APIcall}/>
     </div>
   );
 }
