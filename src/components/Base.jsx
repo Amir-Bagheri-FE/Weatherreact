142a886fe559e59a49ffc1d76937a24c71f21a57
AmirBagheri <amiramb10@gmail.com>
2025-04-29 11:39:31 +0330

    sending Data to WeatherMain

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Details from "./Details";
 import WeatherMain from "./WeatherMain";
 
@@ -5,20 +6,22 @@ import.meta.env.VITE_WEATHER_API_KEY;
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function Base() {
+  const [weatherData, setWeatherData] = useState({});
   async function APIcall(city) {
     try {
       const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
-      const data = await res.json();    
+      const data = await res.json();
+      setWeatherData(data);
     } catch (error) {
       console.error(error, "error occurred");
     }
   }
   return (
     <div className="flex h-[100vh]  w-full bg-cover bg-center bg-no-repeat">
-     <WeatherMain />
-     <Details  APIcall={APIcall}/>
+      <WeatherMain data={weatherData} />
+      <Details APIcall={APIcall} />
     </div>
   );
 }
--- src/components/WeatherMain.jsx
+++ src/components/WeatherMain.jsx
@@ -1,21 +1,27 @@
-function WeatherMain() {
+function WeatherMain({ data }) {
   return (
-    <div
-      id="main"
-      className="flex flex-col justify-between p-1.5 w-[70%] h-full bg-gradient-to-bl from-blue-700 via-blue-900 to-blue-600 bg-opacity-30 rounded-2xl backdrop-blur-md border border-blue-900"
-    >
+    <div id="main" className="flex flex-col justify-between p-1.5 w-[70%] h-full bg-gradient-to-bl from-blue-700 via-blue-900 to-blue-600 bg-opacity-30 rounded-2xl backdrop-blur-md border border-blue-900">
+
       <div className="flex flex-col justify-center items-center w-full h-auto mt-6 py-4">
-        <h2 className="text-gray-700 font-semibold">Please search for a city</h2>
+        <h2 className="text-gray-700 font-semibold "> {data.name || 'please search for a city'} </h2>
         <img
-          src="http://openweathermap.org/img/wn/01d@2x.png"
+          src={`http://openweathermap.org/img/wn/${data?.weather?.[0]?.icon || '01d'}@2x.png`}
           className="animate-pulse mt-2"
           alt="Weather Icon"
         />
       </div>
 
       <div className="w-full h-48 bg-blue-200 bg-opacity-30 backdrop-blur-md mt-4 rounded-lg flex justify-around items-center">
+
         <div className="w-[25%] h-[70%] m-0.5   bg-white rounded-xl shadow-md"></div>
-        <div className="w-[50%] h-[70%] m-0.5 bg-white rounded-xl shadow-md"></div>
+
+        <div className="w-[50%] h-[70%] m-0.5 bg-white rounded-xl shadow-md">
+          <h2>weather type: {data?.weather?.[0]?.description || "N/A"}</h2>
+          <br />
+          <h2>temperature: {data?.main?.temp || "N/A"}°C</h2>
+          <br />
+        </div>
+        
         <div className="w-[25%] h-[70%] m-0.5 bg-white rounded-xl shadow-md"></div>
       </div>
     </div>
