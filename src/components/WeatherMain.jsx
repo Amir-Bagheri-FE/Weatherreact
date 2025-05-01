b3c88e9ab8563cbd6fbecc4bcda55bc0182defd4
AmirBagheri <amiramb10@gmail.com>
2025-05-01 14:25:57 +0330

    icons changed with cdn

--- src/components/WeatherMain.jsx
+++ src/components/WeatherMain.jsx
@@ -1,15 +1,33 @@
 function WeatherMain({ data }) {
- 
+ const weatherIconMap={
+  '01d':'wi-day-sunny',
+  '01n':'wi-night-clear',
+  '02d':'wi-day-cloudy',
+  '02n':'wi-night-alt-cloudy',
+  '03d':'wi-cloud',
+  '03n':'wi-cloud',
+  '04d':'wi-cloudy',
+  '04n':'wi-cloudy',
+  '09d':'wi-showers',
+  '09n':'wi-showers',
+  '10d':'wi-rain',
+  '10n':'wi-rain',
+  '11d':'wi-thunderstorm',
+  '11n':'wi-thunderstorm',
+  '13d':'wi-snow',
+  '13n':'wi-snow',
+  '50d':'wi-fog',
+  '50n':'wi-fog',
+  
+ }
+ const weathericon=weatherIconMap[data?.weather?.[0].icon];
   return (
     <div id="main" className="flex flex-col justify-between p-1  w-[70%] h-full bg-gradient-to-bl from-blue-700 via-blue-900 to-blue-600 bg-opacity-30 rounded-2xl backdrop-blur-md border border-blue-900">
 
       <div className="flex flex-col justify-center items-center w-full h-auto mt-6 py-4">
         <h2 className="text-4xl text-white font-semibold "> {data.name || 'please search for a city'} </h2>
-        <img
-          src={`http://openweathermap.org/img/wn/${data?.weather?.[0]?.icon || '01d'}@2x.png`}
-          className="animate-pulse mt-2"
-          alt="Weather Icon"
-        />
+        
+        <i className={`wi ${weathericon || 'wi-day-sunny'} mt-2 text-3xl text-amber-400 animate-pulse`}></i>
       </div>
 
       <div className="w-full h-48 p-2 bg-blue-200 bg-opacity-30 backdrop-blur-md mt-4 rounded-lg flex justify-around items-center">
