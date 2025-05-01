67e7cf2058a03094f606bab0b55f78c0b656cda9
AmirBagheri <amiramb10@gmail.com>
2025-05-01 15:39:56 +0330

    ref in search bar / bug fixed !

--- src/sections/SearchBar.jsx
+++ src/sections/SearchBar.jsx
@@ -1,4 +1,7 @@
+import { useRef } from "react";
+
 const SearchBar = ({ APIcall }) => {
+  const refTry=useRef(null)
   return (
     <div className="h-[10%]   flex  justify-between">
       <input
@@ -6,10 +9,11 @@ const SearchBar = ({ APIcall }) => {
         placeholder="search for a city"
         className="py-0 px-[90px] focus:border-t-0 focus:border-l-0 focus:border-r-0 focus:border-b-4 border-2 border-green-500 rounded-xl focus:border-green-500 focus:rounded-[8px] focus:outline-none focus:animate-pulse"
         id="CityName"
+        ref={refTry}
       />
       <button
         className="bg-green-500 py-0 px-5 cursor-pointer rounded-[5px]"
-        onClick={()=>{APIcall(document.querySelector('#CityName').value)}}
+        onClick={()=>{APIcall(refTry.current.value)}}
       >
         🔎
       </button>
