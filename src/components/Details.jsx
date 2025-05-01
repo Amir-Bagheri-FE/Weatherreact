c685504eafc50d5f22eb6e7a9e987c7b19266dcb
AmirBagheri <amiramb10@gmail.com>
2025-05-01 14:42:17 +0330

    trend cities weather added

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -6,7 +6,7 @@ function Details({APIcall,Data}) {
   return (
     <div className="p-3 h-full flex flex-col justify-between bg-gradient-to-br from-blue-500 via-blue-400 to-blue-600 rounded-3xl space-y-4">
       <SearchBar APIcall={APIcall}/>
-      <TrendCity />
+      <TrendCity APICall={APIcall}/>
       <Info data={Data}/>
     </div>
   );
--- src/sections/TrendCity.jsx
+++ src/sections/TrendCity.jsx
@@ -1,12 +1,12 @@
-function TrendCity() {
+function TrendCity({APICall}) {
   return (
     <div className="mt-5 pt-1 border-2 border-t-2 border-b-0 border-l-0 border-r-0">
       <h3 className="text-center">Trending Cities</h3>
       <ul>
-        <li className="block font-extrabold m-5">NewYork</li>
-        <li className="block font-extrabold m-5">Florida</li>
-        <li className="block font-extrabold m-5">Paris</li>
-        <li className="block font-extrabold m-5">Madrid</li>
+        <li className="block font-extrabold m-5 cursor-pointer" onClick={(e)=>{APICall(e.target.textContent)}}>New York</li>
+        <li className="block font-extrabold m-5 cursor-pointer" onClick={(e)=>{APICall(e.target.textContent)}}>Florida</li>
+        <li className="block font-extrabold m-5 cursor-pointer" onClick={(e)=>{APICall(e.target.textContent)}}>Paris</li>
+        <li className="block font-extrabold m-5 cursor-pointer" onClick={(e)=>{APICall(e.target.textContent)}}>Madrid</li>
       </ul>
     </div>
   );
